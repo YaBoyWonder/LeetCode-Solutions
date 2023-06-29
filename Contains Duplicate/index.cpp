b7ab1fb0e1b4f2d57622619bcac71ef4ae746081
YaBoyWonder <rahulchalumuri54@gmail.com>
2023-06-29 09:41:16 -0400

    Contains Duplicate - C++

---
+++ Contains Duplicate/index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set; 
+        for (int i=0; i<nums.size(); i++) {
+            if (set.find(nums[i]) != set.end()) {
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
