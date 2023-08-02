f93b954d0288eb7c0015ddde1e8f76b13dfde1ee
YaBoyWonder <rahulchalumuri54@gmail.com>
2023-08-02 16:46:23 -0400

    Maximum Product Subarray - C++

--- Maximum Product Subarray/index.cpp
+++ Maximum Product Subarray/index.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int max_val = INT_MIN, prefix=1, suffix=1;
-        for (int i=0; i<nums.size(); i++) {
-            if (prefix == 0) {prefix = 1;
-                if (suffix == 0) {suffix = 1;}}
+        int arr_length = nums.size();
+        for (int i=0; i<arr_length; i++) {
+            if (prefix == 0) prefix = 1;
+            if (suffix == 0) suffix = 1;
             prefix*=nums[i];
-            suffix*=nums[nums.size()-1-i];
+            suffix*=nums[arr_length - i - 1];
             max_val = max(max_val, max(prefix, suffix));
         }
         return max_val;
